1813c9e243fd9f207abd3ce5477e54991b3b1d5b
Ckyever Gaviola <ckyeverg@gmail.com>
2024-07-07 16:56:54 +1000

    Add ability to check if puzzle is solved

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import slidePuzzle
 
 def main():
     puzzle = slidePuzzle.Board(3)
+    puzzle.board[-1][-1] = puzzle.board[-1][-2]
+    puzzle.board[-1][-2]
     puzzle.print()
+    if puzzle.isSolved():
+        print('This puzzle is solved')
+    else:
+        print('This puzzle is NOT solved')
 
 if __name__ == "__main__":
     main()
--- slidePuzzle.py
+++ slidePuzzle.py
@@ -1,11 +1,33 @@
+import math
 import numpy
 
 class Board:
     def __init__(self, size):
         number = 0
-        self.board = [[number:=number+1 for _ in range(size)] for _ in range(size)]
+        self.size = size
+        self.board = [[number := number+1 for _ in range(size)] for _ in range(size)]
         self.board[-1][-1] = None
+
+    def getSize(self):
+        return self.size
+
+    def getBoard(self):
+        return self.board
 
     def print(self):
         print(numpy.matrix(self.board))
 
+    def isSquareBlank(self, index):
+        return math.pow(self.size, 2)
+
+    def isSolved(self):
+        expectedNumber = 1
+        for row in self.board:
+            for number in row:
+                if number is None and self.isSquareBlank(expectedNumber):
+                    expectedNumber += 1
+                elif number == expectedNumber:
+                    expectedNumber += 1
+                else:
+                    return False
+        return True
