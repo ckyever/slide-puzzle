de9e2e4c92443362754d8fe2668144c74f09a7cb
Ckyever Gaviola <ckyeverg@gmail.com>
2024-07-08 22:50:47 +1000

    Cleanup unit test function names

--- test_slidepuzzle.py
+++ test_slidepuzzle.py
@@ -5,17 +5,17 @@ class Test3x3Board(unittest.TestCase):
     def setUp(self):
         self.puzzle = slidepuzzle.Board(3)
 
-    def test_correct3x3Board(self):
+    def test_correctBoard(self):
         self.assertTrue(self.puzzle.isValid())
 
-    def test_incorrect3x3Board(self):
+    def test_incorrectBoard(self):
         self.puzzle.board[-1][-1] = 9
         self.assertFalse(self.puzzle.isValid())
 
-    def test_solved3x3Board(self):
+    def test_solvedBoard(self):
         self.assertTrue(self.puzzle.isSolved())
 
-    def test_unsolved3x3Board(self):
+    def test_unsolvedBoard(self):
         self.puzzle.board[-1][-2] = None
         self.puzzle.board[-1][-1] = 8
         self.assertFalse(self.puzzle.isSolved())
@@ -24,17 +24,17 @@ class Test4x4Board(unittest.TestCase):
     def setUp(self):
         self.puzzle = slidepuzzle.Board(4)
 
-    def test_correct4x4Board(self):
+    def test_correctBoard(self):
         self.assertTrue(self.puzzle.isValid())
 
-    def test_incorrect4x4Board(self):
+    def test_incorrectBoard(self):
         self.puzzle.board[-1][-1] = 15
         self.assertFalse(self.puzzle.isValid())
 
-    def test_solved4x4Board(self):
+    def test_solvedBoard(self):
         self.assertTrue(self.puzzle.isSolved())
 
-    def test_unsolved4x4Board(self):
+    def test_unsolvedBoard(self):
         self.puzzle.board[-1][-2] = None
         self.puzzle.board[-1][-1] = 15
         self.assertFalse(self.puzzle.isSolved())
