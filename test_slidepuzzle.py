f811c79c540ffd7b3a4439b4d6eb3eb646150318
Ckyever Gaviola <ckyeverg@gmail.com>
2024-07-08 21:57:25 +1000

    Add unit test setup function and add tests for isSolved

--- test_slidepuzzle.py
+++ test_slidepuzzle.py
@@ -1,15 +1,24 @@
 import unittest
 import slidepuzzle
 
-class TestIsValid(unittest.TestCase):
+class TestBoard(unittest.TestCase):
+    def setUp(self):
+        self.puzzle = slidepuzzle.Board(3)
+
     def test_correct3x3Board(self):
-        puzzle = slidepuzzle.Board(3)
-        self.assertTrue(puzzle.isValid())
+        self.assertTrue(self.puzzle.isValid())
 
     def test_incorrect3x3Board(self):
-        puzzle = slidepuzzle.Board(3)
-        puzzle.board[-1][-1] = 9
-        self.assertFalse(puzzle.isValid())
+        self.puzzle.board[-1][-1] = 9
+        self.assertFalse(self.puzzle.isValid())
+
+    def test_solved3x3Board(self):
+        self.assertTrue(self.puzzle.isSolved())
+
+    def test_unsolved3x3Board(self):
+        self.puzzle.board[-1][-2] = None
+        self.puzzle.board[-1][-1] = 8
+        self.assertFalse(self.puzzle.isSolved())
 
 if __name__ == '__main__':
     unittest.main()
