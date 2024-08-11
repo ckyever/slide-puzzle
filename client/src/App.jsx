235af116c603e1f4db07e6ea87cbb158d3b48403
Ckyever Gaviola <ckyeverg@gmail.com>
2024-08-11 21:33:15 +1000

    Redo web app puzzle logic to be client side - add initial styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,57 +24,8 @@ const App = () => {
     fetchPuzzle();
   }, []);
 
-  const handleKeyDown = async (event) => {
-    let direction = '';
-    switch (event.key) {
-      case 'ArrowUp':
-        direction = 'u';
-        break;
-      case 'ArrowRight':
-        direction = 'r';
-        break;
-      case 'ArrowDown':
-        direction = 'd';
-        break;
-      case 'ArrowLeft':
-        direction = 'l';
-        break;
-      default:
-        return;
-    }
-
-    const data = {move: direction};
-    const result = await axios.post("http://localhost:8080/api/puzzle", data);
-    fetchPuzzle();
-  }
-
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    }
-  }, []);
-
-  const handleNewGame = async () => {
-    const result = await axios.post("http://localhost:8080/api/puzzle/new", {});
-    fetchPuzzle();
-  };
-
-  useEffect(() => {
-    const newGameButton = document.getElementById("new-game");
-    newGameButton.addEventListener("click", handleNewGame);
-
-    return () => {
-      newGameButton.removeEventListener("click", newGameButton);
-    }
-  });
-
   return (
-    <div className="container">
-      <Puzzle board={puzzle} size={size}/>
-      <div className={`result ${solved ? 'show' : ''}`}>You solved it!</div>
-      <button id="new-game" className={`new-game ${solved ? 'show' : ''}`}>New Game</button>
+    <div>
     </div>
   )
 };
