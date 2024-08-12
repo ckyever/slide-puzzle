e8265a86c456c820a8485c56d830510f0b1bfe73
Ckyever Gaviola <ckyeverg@gmail.com>
2024-08-12 21:48:26 +1000

    Remove old puzzle react components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,6 @@
 import { useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
-import Puzzle from './components/puzzle/Puzzle';
-
-// CKYTODO: Change this so user interaction with puzzle occurs on the client side
-// CKYTODO: All the python flask server should be going is receiving puzzle states
-// CKYTODO: and returning a list of moves that would solve that puzzle state
 
 const App = () => {
   const [puzzle, setArray] = useState([]);
@@ -25,7 +20,7 @@ const App = () => {
   }, []);
 
   return (
-    <div>
+    <div className="App">
     </div>
   )
 };
