814aceb3f7fa5e340b32d739ab5b14de2fe2b454
Ckyever Gaviola <ckyeverg@gmail.com>
2024-08-17 20:14:35 +1000

    Fix new game button not working when puzzle gets solved

--- client/src/components/winner/Winner.jsx
+++ client/src/components/winner/Winner.jsx
@@ -1,14 +1,14 @@
 import NewGame from '../new-game/NewGame'
 import './Winner.css'
 
-const Winner = ({numbers}) => {
+const Winner = ({numbers, reset}) => {
     if (!numbers.every(n => n.value === n.index + 1))
         return null;
 
     return <div 
         className='winner'>
             <p>You solved it!</p>
-            <NewGame />
+            <NewGame reset={reset}/>
         </div>
 }
 
