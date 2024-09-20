08dbf8424166f2947bdc1c268da27d2ab13ad8b3
Ckyever Gaviola <ckyeverg@gmail.com>
2024-09-20 18:35:43 +1000

    Increase tile slide speed and handle swipes on mobile

--- src/components/tile/Tile.jsx
+++ src/components/tile/Tile.jsx
@@ -3,6 +3,7 @@ import './Tile.css'
 const Tile = ({number, moveTile, numberOfTiles}) =>
     <div 
         onClick={() => moveTile(number)}
+        onTouchEnd={() => moveTile(number)} // When you swipe on mobile
         className={`number ${number.value === numberOfTiles ? 'empty-space' : ''} slot--${number.index}`}>
         {number.value === numberOfTiles ? '' : number.value}
     </div>
